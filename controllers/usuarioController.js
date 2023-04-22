b4479a1ec1bd4b59452c50f3b9a3739fe6191f78
simonhl14 <simonh419@gmail.com>
2023-04-22 10:15:35 -0500

    Validaciones del registro

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+import { check, validationResult } from 'express-validator'
 import Usuario from '../models/Usuario.js'
 
 const formularioLogin = (req, res) => {
@@ -13,6 +14,26 @@ const formularioRegistro = (req, res) => {
 }
 
 const registrar = async (req, res) => {
+    // Validacion
+    await check('nombre').notEmpty().withMessage('El nombre es obligatorio').run(req);
+    await check('email').isEmail().withMessage('El email es incorrecto').run(req);
+    await check('password').isLength({ min: 6 }).withMessage('La contraseña debe ser de al menos 6 caracteres').run(req);
+    await check('repetir_password').equals(req.body.password).withMessage('Las contraseñas no son iguales').run(req);
+
+    let resultado = validationResult(req);
+
+    // Verificar si esta vacio
+    if (!resultado.isEmpty()) {
+        return res.render('auth/registro', { 
+            pagina: 'Crear cuenta',
+            errores: resultado.array(),
+            usuario: {
+                nombre: req.body.nombre,
+                email: req.body.email,
+            }
+        });
+    }
+
     const usuario = await Usuario.create(req.body);
     res.json(usuario);
 }
